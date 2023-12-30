92d3fb086b3245c7366d8520391f04fce2ee9f64
magentamicah <152821518+magentamicah@users.noreply.github.com>
2023-12-30 13:34:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
+const strArr = ["a", "b", "c", "d", "e"];
+const numArr = [1, 2, 3, 4, 5];
+const objArr = [{id: 1}, {id: 2}, {id: 3}, {id: 4}];
+const arrArr = [[{id: 1}, {id: 2}, {id: 3}, {id: 4}], [1, 2, 3, 4, 5], ["a", "b", "c", "d", "e"] ];
 console.log("Script is loading fine meow.");
